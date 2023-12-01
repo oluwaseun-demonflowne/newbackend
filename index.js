2b4f8c944397b15ff6fc8067c37fe00b97cbcbdd
demonflowne <ibuemmanuel60@gmail.com>
2023-12-01 16:19:25 +0100

    Test repository

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => res.send('Hello World!' ))
+  
+  
+
+
+app.listen(PORT, () => console.log(`Server up at PORT:${PORT}`))
